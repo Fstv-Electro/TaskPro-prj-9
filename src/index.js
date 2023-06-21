0616c7f604088997c33bf2e64385503f51e1412b
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-06-21 22:07:47 +0300

    add files for redux and browserRouter

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basenmae="/taskpro-prj-9">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
