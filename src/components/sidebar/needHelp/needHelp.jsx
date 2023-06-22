dfa3e5eeab3c422a5b1d95257ca0dcafbe71c749
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-06-22 22:57:19 +0300

    add images, sprites, styles for component nnedHelp

--- src/components/sidebar/needHelp/needHelp.jsx
+++ src/components/sidebar/needHelp/needHelp.jsx
@@ -1,13 +1,17 @@
-import { Container, Text, Span, Button } from "./needHelp.styled";
+import { Container, Text, Span, Button, Image } from "./needHelp.styled";
+import background from "../../../images/2.png";
 
 export const NeedHelp = () => {
     return (
         <Container>
+          <Image src={background} alt="Logo" />
             <Text>
             If you need help with <Span>TaskPro</Span>, 
             check out our support resources or reach out to our customer support team.
             </Text>
-            <Button>Need help?</Button>
+            <Button>
+                Need help?
+            </Button>
         </Container>
     )
 }
--- src/components/sidebar/needHelp/needHelp.styled.js
+++ src/components/sidebar/needHelp/needHelp.styled.js
@@ -1,19 +1,24 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-    width: 212px;
-    height: 272px;
+    max-width: 197px;
     padding: 20px;
     margin-bottom: 20px;
     border-radius: 8px;
     background: #1F1F1F;
+    @media (min-width: 768px) {
+        max-width: 212px;
+      }
 `;
 
 export const Text = styled.p`
     color: #FFF;
-    font-size: 14px;
+    font-size: 12px;
     font-family: "Poppins";
     line-height: 20px;
+    @media (min-width: 768px) {
+        font-size: 14px;
+      }
 `;
 
 export const Span = styled.span`
@@ -26,4 +31,12 @@ export const Button = styled.button`
     border: none;
     color: #FFF;
     background: #1F1F1F;
+    @media (min-width: 768px) {
+        font-size: 14px;
+      }
 `;
+export const Image = styled.img`
+    width: 54px;
+    height: 78px;
+    margin-bottom: 14px;
+`
