ae1329d540c51dbb4565bbf21f767235830fc639
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-06-22 18:26:01 +0300

    check build

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/taskpro-prj-9">
+      <BrowserRouter basename="/TaskPro-prj-9">
         <App />
       </BrowserRouter>
     </Provider>
