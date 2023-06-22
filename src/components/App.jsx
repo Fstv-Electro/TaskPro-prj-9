3a815c9c245c1afcb25556cad752213405bf513e
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-06-23 00:35:46 +0300

    Welcome page Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { WelcomePage } from './welcomePage/welcomePage';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // fontSize: 40,
+        // color: '#010101',
       }}
     >
-      React task PRO
+      <WelcomePage />
+      {/* React task PRO */}
     </div>
   );
 };
