b276cbb538c5b6876985ef3d4cda8498d7ca3fe9
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-06-24 16:54:06 +0300

    chanhe styles in sprite, add universal modal

--- src/components/sidebar/needHelp/needHelp.jsx
+++ src/components/sidebar/needHelp/needHelp.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Container,
   Text,
@@ -5,11 +6,18 @@ import {
   Button,
   Image,
   TextButton,
+  Svg
 } from './needHelp.styled';
 import background from '../../../images/help.png';
 import sprite from '../../../images/symbol-defs.svg';
+import { Modal } from 'components/modal/modal';
 
 export const NeedHelp = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(isOpen => !isOpen)
+  }
   return (
     <Container>
       <Image src={background} alt="Logo" />
@@ -17,12 +25,13 @@ export const NeedHelp = () => {
         If you need help with <Span>TaskPro</Span>, check out our support
         resources or reach out to our customer support team.
       </Text>
-      <Button>
-        <svg aria-label="question with round" width="32px" height="32px">
+      <Button onClick={toggleModal}>
+        <Svg aria-label="question with round" width="20px" height="20px">
           <use href={sprite + '#icon-help-circle'}></use>
-        </svg>
+        </Svg>
         <TextButton>Need help?</TextButton>
       </Button>
+      {isOpen && <Modal onClose={toggleModal} children={<h2>Hello</h2>} />}
     </Container>
   );
 };
