c955e9274da73c94a2db682dd6e46f418b3ed55e
Roman <korzunroman56@gmail.com>
2023-06-26 19:38:51 +0300

    убрал консоль

--- src/components/routs/restrictedRoute.js
+++ src/components/routs/restrictedRoute.js
@@ -4,6 +4,6 @@ import { Navigate } from 'react-router-dom';
 
 export const RestrictedRoute = ({ component, redirectTo }) => {
   const isLoggedIn = useSelector(selectIsLogin);
-  console.log(isLoggedIn);
+
   return isLoggedIn ? <Navigate to={redirectTo} /> : component;
 };
