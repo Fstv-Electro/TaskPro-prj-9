5553021e0f1acb8f4494828114ed080b467afe36
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-06-28 00:13:48 +0300

    Update selectores.js

--- src/redux/auth/selectores.js
+++ src/redux/auth/selectores.js
@@ -1,5 +1,3 @@
 export const selectIsLogin = state => state.auth.isLogin;
-
-export const selectIsError = state => state.auth.isError;
-
 export const selectUser = state => state.auth.user;
+export const selectIsError = state => state.auth.isError;
