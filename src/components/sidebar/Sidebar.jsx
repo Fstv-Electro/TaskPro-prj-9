66bd329ef823045283056dcf18de5ba7ca7f147a
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-06-27 19:05:26 +0300

    CreateBoard

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { CreateBoard } from './createBoard/CreateBoard';
+import { CreateBoard } from './CreateBoard/CreateBoard';
 import { MyBoard } from './myBoard/MyBoard';
 import { LogOut } from './logOut/LogOut';
 import { NeedHelp } from './needHelp/needHelp';
