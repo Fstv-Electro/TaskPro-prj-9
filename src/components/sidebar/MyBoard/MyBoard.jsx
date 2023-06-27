47c5690c27bf1196ca5a36f74750c2f5a63102a1
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-06-27 18:32:01 +0300

    fix/bug

--- src/components/sidebar/MyBoard/MyBoard.jsx
+++ src/components/sidebar/MyBoard/MyBoard.jsx
@@ -1,35 +1,35 @@
-// import {
-//   Item,
-//   WrapperProjectOffice,
-//   Project,
-//   WrapperProjectIcons,
-//   IconButton,
-// } from './MyBoard.styled';
-// import sprite from '../../../images/symbol-defs.svg';
+import {
+  Item,
+  WrapperProjectOffice,
+  Project,
+  WrapperProjectIcons,
+  IconButton,
+} from './MyBoard.styled';
+import sprite from '../../../images/symbol-defs.svg';
 
-// export const MyBoard = () => {
-//   return (
-//     <list>
-//       <Item>
-//         <WrapperProjectOffice onClick={() => {}}>
-//           <svg aria-label="question with round" width="18px" height="16px">
-//             <use href={sprite + '#icon-Project'}></use>
-//           </svg>
-//           <Project>Project office</Project>
-//         </WrapperProjectOffice>
-//         <WrapperProjectIcons>
-//           <IconButton onClick={() => {}}>
-//             <svg aria-label="question with round" width="16px" height="16px">
-//               <use href={sprite + '#icon-pencil-01'}></use>
-//             </svg>
-//           </IconButton>
-//           <IconButton onClick={() => {}}>
-//             <svg aria-label="question with round" width="16px" height="16px">
-//               <use href={sprite + '#icon-trash-04'}></use>
-//             </svg>
-//           </IconButton>
-//         </WrapperProjectIcons>
-//       </Item>
-//     </list>
-//   );
-// };
+export const MyBoard = () => {
+  return (
+    <list>
+      <Item>
+        <WrapperProjectOffice onClick={() => {}}>
+          <svg aria-label="question with round" width="18px" height="16px">
+            <use href={sprite + '#icon-Project'}></use>
+          </svg>
+          <Project>Project office</Project>
+        </WrapperProjectOffice>
+        <WrapperProjectIcons>
+          <IconButton onClick={() => {}}>
+            <svg aria-label="question with round" width="16px" height="16px">
+              <use href={sprite + '#icon-pencil-01'}></use>
+            </svg>
+          </IconButton>
+          <IconButton onClick={() => {}}>
+            <svg aria-label="question with round" width="16px" height="16px">
+              <use href={sprite + '#icon-trash-04'}></use>
+            </svg>
+          </IconButton>
+        </WrapperProjectIcons>
+      </Item>
+    </list>
+  );
+};
