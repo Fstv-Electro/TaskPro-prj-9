6fced5e9924ab5f6ebfc8a3996a740bc2e5fc7ea
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-06-30 12:52:34 +0300

    delete console.log

--- src/pages/ScreenPage.js
+++ src/pages/ScreenPage.js
@@ -1,16 +1,7 @@
 import { DashboardHeader } from 'components/HeaderDashboard/HeaderDashboard';
 import { DashboardMain } from 'components/MainDashboard/MainDashboard.styled';
-import { EmptyPage } from 'components/HomeEmptyPage/HomeEmptyPage';
-import { useSelector } from 'react-redux';
-import { selectBoard } from 'redux/dashboards/selectors';
 
 export default function ScreenPage() {
-  const boards = useSelector(selectBoard);
-  console.log(boards);
-  if (boards.length === 0) {
-    return <EmptyPage />;
-  }
-
   return (
     <>
       <DashboardHeader />
