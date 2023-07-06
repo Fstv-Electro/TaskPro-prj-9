25740084bd5e37274fe9fdf6fc68838af24ed3f7
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-07-06 13:10:48 +0300

    fix space

--- src/components/HomeEmptyPage/HomeEmptyPage.jsx
+++ src/components/HomeEmptyPage/HomeEmptyPage.jsx
@@ -5,7 +5,7 @@ export const EmptyPage = () => {
         <Container>
             <InfoText>
                 Before starting your project, it is essential
-                <AccentText>to create a board</AccentText> to visualize and track all the necessary tasks and milestones.
+                <AccentText> to create a board</AccentText> to visualize and track all the necessary tasks and milestones.
                 This board serves as a powerful tool to organize the workflow and ensure effective collaboration among team members.
             </InfoText>
         </Container>
